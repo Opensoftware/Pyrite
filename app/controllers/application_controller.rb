0b473ef87a5d6fab1a6eef29a1dfec48c290d226
Robert Mitwicki <mitfik@gmail.com>
2013-02-27 22:31:05 +0100

    Add current_semester helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,47 +11,41 @@ class ApplicationController < ActionController::Base
   rescue_from(::ActionController::UnknownAction) {redirect_to :controller => 'main' }
   protect_from_forgery
 
+  helper_method :current_semester
+
 
 # metoda pobiera informacje na temat obecnie ustawionego planu zajęć
 # return @baza - identyfikator oznaczający nazwę bazy danych na której operuje system
   def get_current_plan
-		#	@config = Setting.find(:first)
-		#	case @config.current_plan
-		#		when 0 : @baza = OldPlan
-	  #		when 1 : @baza = Plan
-  	#		when 2 : @baza = NewPlan
-		#	end
-				 @baza = Plan
-     return @baza
-	end
+    #   @config = Setting.find(:first)
+    #   case @config.current_plan
+    #       when 0 : @baza = OldPlan
+    #     when 1 : @baza = Plan
+    #       when 2 : @baza = NewPlan
+    #   end
+    @baza = Plan
+    return @baza
+  end
 
-#pobranie aktualnego roku akademickiego
-	def semestr
-		if 	DateTime.now.month < 9
-			@rok = DateTime.now.year-1
-		else
-			@rok = DateTime.now.year
-		end
-		return @rok
-	end
+  # pobranie aktualnego roku akademickiego
+  def current_semester
+    Schedule.find_by_id(Setting.first.try(:current_plan))
+  end
 
-# pobieranie id planów ustawionych do edycji
-#
+  # pobieranie id planów ustawionych do edycji
+  def getEditPlan
+    return Setting.find(1).plan_to_edit
+  end
 
-	def getEditPlan
-		return Setting.find(1).plan_to_edit
-	end
-
-# pobranie id planu ustawionego do przeglądania
-
-	def getViewPlan
-		return Setting.find(1).current_plan
-	end
+  # pobranie id planu ustawionego do przeglądania
+  def getViewPlan
+    return Setting.find(1).current_plan
+  end
 
 
-# pobranie bloków dla grup
-# bloki - tablica wpisów z bazy danych zawierająca wpisy dla grup
-	def getblocs(bloki)
+  # pobranie bloków dla grup
+  # bloki - tablica wpisów z bazy danych zawierająca wpisy dla grup
+  def getblocs(bloki)
     for pl in bloki
       @Gz = (pl.godz.to_i / 60)
       @Mz = pl.godz.to_i - @Gz.minutes
@@ -60,18 +54,19 @@ class ApplicationController < ActionController::Base
       else
         @GMz = "#{@Gz}:#{@Mz}"
       end
-			@gr = ""
-			for g in pl.groups
-				@gr += g.nazwa
-				@gr += " "
-			end
+      @gr = ""
+      for g in pl.groups
+        @gr += g.nazwa
+        @gr += " "
+      end
       @qwe[@GMz][pl.dni] = "#{pl.prowadzacy}::s.#{ pl.sala}::#{pl.dlugosc}h::#{pl.przedmiot}::#{pl.rodzaj}::ID:#{pl.id}::#{pl.budynek}"
     end
-		return @qwe
-	end
-# pobieranie bloków dla sal
-# bloki - tablica wpisów z bazy danych zawierające wpisy dla sal
-	def getblocsroom(bloki)
+    return @qwe
+  end
+
+  # pobieranie bloków dla sal
+  # bloki - tablica wpisów z bazy danych zawierające wpisy dla sal
+  def getblocsroom(bloki)
     for pl in bloki
       @Gz = (pl.godz.to_i / 60)
       @Mz = pl.godz.to_i - @Gz.minutes
@@ -80,18 +75,19 @@ class ApplicationController < ActionController::Base
       else
         @GMz = "#{@Gz}:#{@Mz}"
       end
-			@gr = ""
-			for g in pl.groups
-				@gr += g.nazwa
-				@gr += " "
-			end
+      @gr = ""
+      for g in pl.groups
+        @gr += g.nazwa
+        @gr += " "
+      end
       @qwe1[@GMz][pl.dni] = "#{pl.prowadzacy}::gr. #{@gr}::#{pl.dlugosc}h::#{pl.przedmiot}::#{pl.rodzaj}::ID:#{pl.id}::#{pl.budynek}"
     end
-		return @qwe1
-	end
-# pobieranie bloków z pełnymi informacjami
-# bloki - tablica wpisów z bazy danych zawierająca wpisy dla planów
-	def getblocsall(bloki)
+    return @qwe1
+  end
+
+  # pobieranie bloków z pełnymi informacjami
+  # bloki - tablica wpisów z bazy danych zawierająca wpisy dla planów
+  def getblocsall(bloki)
     for pl in bloki
       @Gz = (pl.godz.to_i / 60)
       @Mz = pl.godz.to_i - @Gz.minutes
@@ -100,56 +96,55 @@ class ApplicationController < ActionController::Base
       else
         @GMz = "#{@Gz}:#{@Mz}"
       end
-			@gr = ""
-			for g in pl.groups
-				@gr += g.nazwa
-				@gr += " "
-			end
+      @gr = ""
+      for g in pl.groups
+        @gr += g.nazwa
+        @gr += " "
+      end
       @qwe[@GMz][pl.dni] = "#{pl.prowadzacy}::gr. #{@gr}::s. #{pl.sala}::#{pl.dlugosc}h::#{pl.przedmiot}::#{pl.sala}::#{pl.budynek}::#{pl.rodzaj}::ID:#{pl.id}::."
     end
-		return @qwe
-	end
+    return @qwe
+  end
 
-# pobranie informacji na temat okresów w których plan zajęc nie obowiązuje
-# date - zmienna reprezentujaca date która chcemy przyrównać (sprawdzić) do okresów
-	def checkfreetime(date)
-		@free = Freeday.find(:all)
-		for i in @free
+  # pobranie informacji na temat okresów w których plan zajęc nie obowiązuje
+  # date - zmienna reprezentujaca date która chcemy przyrównać (sprawdzić) do okresów
+  def checkfreetime(date)
+    @free = Freeday.find(:all)
+    for i in @free
 
-			if 	(Date.parse(i.start.strftime("%Y-%m-%d %H:%M %p")) <= Date.parse(date) && Date.parse(date) <= Date.parse(i.stop.strftime("%Y-%m-%d %H:%M %p")))
-					puts "++++"
-					return false
-			end
-		end
-		return true
-	end
+      if  (Date.parse(i.start.strftime("%Y-%m-%d %H:%M %p")) <= Date.parse(date) && Date.parse(date) <= Date.parse(i.stop.strftime("%Y-%m-%d %H:%M %p")))
+        puts "++++"
+        return false
+      end
+    end
+    return true
+  end
 
-# metoda zamieniająca angielską nazwe dnia tygodnia na polską
-# day - trzy literowa nazwa dnia tygodnia po angielsu
-# return @day polska nazwa dnia tygodnia
-	def getpolishname(day)
-			case day
-				when "Mon"
-          @day = "pon"
-				when "Tue"
-          @day = "wto"
-				when "Wed"
-          @day = "sro"
-				when "Thu"
-          @day = "czw"
-				when "Fri"
-          @day = "pia"
-				when "Sat"
-          @day = "sob"
-				when "Sun"
-          @day = "nie"
-			end
-		return @day
-	end
+  # metoda zamieniająca angielską nazwe dnia tygodnia na polską
+  # day - trzy literowa nazwa dnia tygodnia po angielsu
+  # return @day polska nazwa dnia tygodnia
+  def getpolishname(day)
+    case day
+    when "Mon"
+      @day = "pon"
+    when "Tue"
+      @day = "wto"
+    when "Wed"
+      @day = "sro"
+    when "Thu"
+      @day = "czw"
+    when "Fri"
+      @day = "pia"
+    when "Sat"
+      @day = "sob"
+    when "Sun"
+      @day = "nie"
+    end
+    return @day
+  end
 
-# metoda sprawdza występowanie danego bloku w planie zajęć i rezerwacji
-#
-	def GetConfict()
+  # metoda sprawdza występowanie danego bloku w planie zajęć i rezerwacji
+  def GetConfict()
     for s in @sp
       @dod = s.dlugosc.to_i * 3
       @godz1 = s.godz.to_i
@@ -157,16 +152,16 @@ class ApplicationController < ActionController::Base
       @spr1 = Array.new(@kom2)
       @kom2.times { |i| @spr1[i] = @godz1 + ( i * 15 ) }
       @kom2.times { |j| @kom1.times { |i|
-         if (@spr[i] == @spr1[j] )
-           @jest = 1
-         end }}
+        if (@spr[i] == @spr1[j] )
+          @jest = 1
+        end }}
     end
-		return @jest
-	end
+    return @jest
+  end
 
-#zamienienie wartości przechowywane w bazie na godzinę
-# gz - int - odpowiednik godziny w bazie np( 600 => 10:00 )
- 	def GetHour(gz)
+  #zamienienie wartości przechowywane w bazie na godzinę
+  # gz - int - odpowiednik godziny w bazie np( 600 => 10:00 )
+  def GetHour(gz)
     @Gz = (gz.to_i / 60)
     @Mz = gz.to_i - @Gz.minutes
     if (@Mz == 0 )
@@ -174,6 +169,6 @@ class ApplicationController < ActionController::Base
     else
       @GMz = "#{@Gz}:#{@Mz}"
     end
-		@GMz
-	end
+    @GMz
+  end
 end
