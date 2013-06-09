4be28a3caec3d8397619813a3e3c4d1c2ece3316
Robert Mitwicki <mitfik@gmail.com>
2013-06-10 00:05:25 +0200

    Monkey patch for name in group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,9 @@ class Group < ActiveRecord::Base
 	has_and_belongs_to_many :plans
 	has_and_belongs_to_many :reservations
 	has_and_belongs_to_many :blocks
+
+  # TODO remove when model will be updated
+  def name
+    self.nazwa
+  end
 end
