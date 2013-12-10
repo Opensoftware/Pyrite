9eb528fe04f7ce4b144cf79db2d331a11782a594
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2013-12-10 02:36:14 +0100

    Fix converting blocks to events for fullcalendar

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -2,7 +2,9 @@ module BlocksHelper
   def convert_blocks_to_events(blocks)
     events = []
     blocks.each do |block|
-      events << { id: block.id, title: block.name, start: block.start, end: block.end, allDay: false}
+      block.dates.each do |date|
+        events << { id: block.id, title: block.name, start: date.start_date, end: date.end_date, allDay: false}
+      end
     end
     return events
   end
