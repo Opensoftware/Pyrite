50f00cc4df6f48395b73682397e7b7944270f762
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2013-12-10 02:34:22 +0100

    Fix block event relation

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,7 +2,7 @@ class Block < ActiveRecord::Base
   belongs_to :room
   belongs_to :lecturer
   belongs_to :building
-  belongs_to :event
+  belongs_to :event, :class_name => "AcademicYear::Event"
   belongs_to :block_type
   has_many :dates
 
