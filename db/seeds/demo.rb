b84d840d0d47331324efbcff3cb755467801b52a
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2013-12-16 14:08:35 +0100

    Improve seeds and add siatka rake
    Siatka rake will be used to prepare demo app.
    
    Change-Id: Ia46c0fc35bdf24924442800fe89ae47f32aad0c0

--- db/seeds/demo.rb
+++ db/seeds/demo.rb
@@ -1,8 +1,4 @@
 User.destroy_all
 User.create(:email => "siatka@opensoftware.pl", :password => "123qweasdzxc", :password_confirmation => "123qweasdzxc")
 
-User.first.has_role "admin"
-User.first.has_role "moderator"
-User.first.has_role "user"
-
-Settings.create(:unit_name: "Siatka-DEMO", :email_contact => "contact@opensoftware.pl")
+Settings.create(:unit_name => "Siatka-DEMO", :email_contact => "contact@opensoftware.pl")
