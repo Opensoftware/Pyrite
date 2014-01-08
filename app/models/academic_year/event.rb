1e1226c113409c92d63ed9edd34b5ee0ed7b809f
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-01-09 00:20:00 +0100

    Display academic events only for configured academic year.
    Change-Id: Id1463282ea7602fb545be428f934448677062910

--- app/models/academic_year/event.rb
+++ app/models/academic_year/event.rb
@@ -2,4 +2,8 @@ class AcademicYear::Event < ActiveRecord::Base
   attr_accessible :end_date, :lecture_free, :name, :start_date
   belongs_to :academic_year
   has_many :blocks
+
+
+  scope :current_for_edit, where(:academic_year_id => Settings.plan_to_edit)
+  scope :currnet_for_view, where(:academic_year_id => Settings.plan_to_view)
 end
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,3 +1,23 @@
 class Settings < ActiveRecord::Base
   attr_accessible :current_plan, :plan_to_edit, :email_contact, :unit_name
+
+
+  class << self
+    # If plan_to_edit is not set, default one is the first found.
+    def plan_to_edit
+      Settings.first.plan_to_edit || AcademicYear.first.try(:id)
+    end
+
+    def plan_to_view
+      Settings.first.current_plan
+    end
+
+    def email_contact
+      Settings.first.email_contact
+    end
+
+    def unit_name
+      Settings.first.unit_name
+    end
+  end
 end
