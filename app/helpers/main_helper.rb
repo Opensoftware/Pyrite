c9e47633122f63e33d3cd57a9819cefa80f29cb8
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-01-08 09:38:22 +0100

    Add cache for dashboard.
    Change-Id: I4d06c33819775227e1ebe3413f4744312ace59e8

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,3 +1,23 @@
 module MainHelper
 
+
+  def cache_key_for_rooms_with_url
+    count          = Room.count
+    max_updated_at = Room.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "rooms/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_groups_with_url
+    count          = Group.count
+    max_updated_at = Group.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "groups/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_timetable_forms
+    count          = Group.count
+    max_group_updated_at = Group.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    count          += Room.count
+    max_room_updated_at = Room.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "timetable-forms/all-#{count}-#{max_group_updated_at}-#{max_room_updated_at}"
+  end
 end
