38d753bdfc14f375120f6de30ee31a4cdf130129
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-01-08 07:23:49 +0100

    Improve demo seeds
    Change-Id: Ia8bb9e14211ec19082d961e82f1f7b5422454fe6

--- db/seeds/demo.rb
+++ db/seeds/demo.rb
@@ -19,3 +19,12 @@ Room.destroy_all
 Room.create(:name => "02", :room_type_id => room_type1.id)
 Room.create(:name => "03", :room_type_id => room_type2.id)
 Room.create(:name => "04", :room_type_id => room_type3.id)
+
+AcademicYear.destroy_all
+academic_year = AcademicYear.create(:name => "2013/2014", :start_date => "2014-09-01", :end_date => "2015-06-30")
+AcademicYear::Event.destroy_all
+academic_year.events.build(:name => "Semestr Zimowy", :start_date => "2014-09-01", :end_date => "2015-02-01")
+academic_year.events.build(:name => "Semestr Letni", :start_date => "2015-03-01", :end_date => "2015-06-01")
+academic_year.events.build(:name => "Sesja zimowa", :start_date => "2015-02-02", :end_date => "2014-06-01", :lecture_free => true)
+academic_year.events.build(:name => "Sesja letnia", :start_date => "2015-06-02", :end_date => "2014-06-30", :lecture_free => true)
+academic_year.save
