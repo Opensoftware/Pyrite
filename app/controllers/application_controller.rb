0d47cb3d0a341684c2bf534c97a2bfec3497a5ed
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-03-02 21:54:04 +0100

    Improve flash messages
    Change-Id: I8941aa22264d99efd513225fd90e3ded30dc8b2d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  protect_from_forgery
-  helper_method :flash_messages
-
-  def flash_messages
-    flash[:notice] || flash[:error] || nil
-  end
-
   def after_sign_in_path_for(resource)
     dashboard_index_path
   end
