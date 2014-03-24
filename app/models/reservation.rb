aacb6e471c98bd0cbf53e55825d429ee4b197c68
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-03-24 21:22:48 +0100

    Remove unused model
    Change-Id: I663be7794329f3a18ff194d5cd011b17b472e980

--- app/models/reservation.rb
+++
@@ -1,4 +0,0 @@
-class Reservation < ActiveRecord::Base
-#	has_and_belongs_to_many :groups
-  attr_accessible :godz, :prowadzacy, :dlugosc, :rodzaj, :przedmiot, :waznosc, :grupa, :uwagi, :sala, :dni
-end
