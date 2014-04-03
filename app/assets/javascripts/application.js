516330d4b60368a131fe6caaa5797a9ee44845b2
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-04-03 23:02:12 +0200

    Update jquery to 1.11.0
    Change-Id: I9c1dd9b7a634742badd680475ccbdcfc9584edfd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-1.10.2
+//= require jquery-1.11.0
 //= require jquery-ui-1.10.4.custom
 //= require rails
 //= require bootstrap/js/bootstrap
