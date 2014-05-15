1e48558ac5a7bc41a13deead7f845bc3fa74ae41
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-05-16 00:32:11 +0200

    Remove validation for the building
    Change-Id: I938f7e2fcb9f202f8029213853a172ed7cc1a7c4

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,13 +2,5 @@ class Building < ActiveRecord::Base
   has_many :rooms, :dependent => :destroy
 
   validates :name, :address, :presence => true
-  validate :coordinates
 
-  private
-
-    def coordinates
-      if self.latitude.blank? or self.longitude.blank?
-        errors.add(:base, I18n.t("error_missing_coordinates"))
-      end
-    end
 end
