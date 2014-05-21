cc8b5132a5ebe466d913c84dfb340dadd349296f
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-05-21 19:46:11 +0200

    Move navigations into pyrit namespace
    Include new path into simple navigations settings and rename top header
    navigation.
    
    Change-Id: I10f5f54a7c4d09feed0beb158a52decbc1a7afc0

--- lib/pyrite/engine.rb
+++ lib/pyrite/engine.rb
@@ -1,5 +1,8 @@
+require 'simple-navigation'
 module Pyrite
   class Engine < ::Rails::Engine
     isolate_namespace Pyrite
+    path = File.join(Pyrite::Engine.root, "app", "navigations", "pyrite")
+    SimpleNavigation.config_file_paths << path
   end
 end
