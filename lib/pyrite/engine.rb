322979226f887d6d5f0b17dde53aee9da21223a0
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-05-22 21:30:13 +0200

    Add assets for precompilation which are not part of the manifest
    Change-Id: I469e397b6b873b8820bd03f79d0f972751165a4e

--- lib/pyrite/engine.rb
+++ lib/pyrite/engine.rb
@@ -4,5 +4,20 @@ module Pyrite
     isolate_namespace Pyrite
     path = File.join(Pyrite::Engine.root, "app", "navigations", "pyrite")
     SimpleNavigation.config_file_paths << path
+
+    initializer "pyrite.assets.precompile" do |app|
+      app.config.assets.precompile += ["pyrite/OpenLayers.js", "pyrite/spectrum.js", "pyrite/blocks.js",
+                                                     "pyrite/blocks/new.js", "pyrite/timetable_forms.js",
+                                   "pyrite/buildings.js", "pyrite/buildings/edit.js",
+                                   "pyrite/buildings/show.js", "pyrite/blocks/new_part_time.js",
+                                   "pyrite/block/types.js", "pyrite/academic_years.js",
+                                   "pyrite/settings.js", "pyrite/reservations.js",
+                                   "pyrite/buildings.css", "pyrite/blocks.css",
+                                   "pyrite/block/types.css", "pyrite/spectrum.css", "pyrite/theme/default/style.css",
+                                   "pyrite/theme/default/img/*", "pyrite/theme/default/google.css",
+                                   "pyrite/theme/default/style.mobile.css", "pyrite/theme/default/ie6-style.css",
+                                   "pyrite/chosen-sprite.png"
+      ]
+    end
   end
 end
