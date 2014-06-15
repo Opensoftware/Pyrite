3b9f048f00e992b9de3249d6b619e9eb5a05a259
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-06-15 19:09:43 +0200

    Fix navigation for non logged users
    Change-Id: Ie6a02a2ce2450ce637ea6faa027d5be271e5969a

--- app/navigations/pyrite/pyrite_common_navigation.rb
+++ app/navigations/pyrite/pyrite_common_navigation.rb
@@ -1,8 +1,8 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav navbar-nav'
-    primary.item :pyrite, "<i class='icon-callendar-white'></i> #{t('pyrite.title_system_name')}", main_app.dashboard_path
     if current_user
+      primary.item :pyrite, "<i class='icon-callendar-white'></i> #{t('pyrite.title_system_name')}", main_app.dashboard_path
       primary.item :user_account, "<i class='icon-user-white'></i> #{current_user.verifable.surname_name}", main_app.user_path(current_user) do |primary|
         primary.item :nav, t(:label_account_settings), main_app.edit_user_path(current_user)
         if department_settings
@@ -11,6 +11,7 @@ SimpleNavigation::Configuration.run do |navigation|
         primary.item :logout, t(:label_logout), main_app.logout_path, method: :delete
       end
     else
+      primary.item :pyrite, "<i class='icon-callendar-white'></i> #{t('pyrite.title_system_name')}", main_app.root_path
       primary.item :login, I18n.t(:label_login), main_app.new_user_session_path
     end
   end
