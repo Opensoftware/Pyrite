4e975ff5e71cbb3606819de52f77bc2781f4b099
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-06-15 02:02:20 +0200

    Rename header navigation to common
    Change-Id: Ib943a67b1c192de7a5cd63890026a9d95ef980e5

---
+++ app/navigations/pyrite/pyrite_common_navigation.rb
@@ -0,0 +1,17 @@
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.items do |primary|
+    primary.dom_class = 'nav navbar-nav'
+    primary.item :pyrite, "<i class='icon-callendar-white'></i> #{t('pyrite.title_system_name')}", main_app.dashboard_path
+    if current_user
+      primary.item :user_account, "<i class='icon-user-white'></i> #{current_user.verifable.surname_name}", main_app.user_path(current_user) do |primary|
+        primary.item :nav, t(:label_account_settings), main_app.edit_user_path(current_user)
+        if department_settings
+          primary.item :nav, t(:label_thesis_plural_count), main_app.edit_department_setting_path(department_settings), :if => lambda { can?(:manage, DepartmentSettings) }
+        end
+        primary.item :logout, t(:label_logout), main_app.logout_path, method: :delete
+      end
+    else
+      primary.item :login, I18n.t(:label_login), main_app.new_user_session_path
+    end
+  end
+end
