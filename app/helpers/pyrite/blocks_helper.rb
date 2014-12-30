f7ca52a8f760cf21aedaa308611aa23bdea9d2ed
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-12-30 23:54:14 +0100

    Show comments inside the block.
    We are using native html title attribute, as the bootstrap tooltip will not work with
    overview: hidden (all elements in fullcalendar are using it to show blocks
    correctly. )
    
    The comment will be shown on top right corner of the block with question mark
    icon.
    
    Change-Id: Ie909e191ef2ee11a446bd07933c4128208848c23

--- app/helpers/pyrite/blocks_helper.rb
+++ app/helpers/pyrite/blocks_helper.rb
@@ -8,7 +8,8 @@ module Pyrite
                    room_name: block.rooms_names, block_type: block.variant_name,
                    group_names: block.group_names, lecturer: block.lecturer_name,
                    url: edit_block_path(block), delete_url: block_path(block),
-                   move_url: move_block_path(block), resize_url: resize_block_path(block)
+                   move_url: move_block_path(block), resize_url: resize_block_path(block),
+                   comments: block.comments
           }
         end
         sum
@@ -21,7 +22,8 @@ module Pyrite
           sum << { id: block.id, title: block.name, start: date.start_date, end: date.end_date,
                    allDay: false, backgroundColor: block.variant.try(:color),
                    room_name: block.rooms_names, block_type: block.variant_name,
-                   group_names: block.group_names, lecturer: block.lecturer_name
+                   group_names: block.group_names, lecturer: block.lecturer_name,
+                   comments: block.comments
           }
         end
         sum
